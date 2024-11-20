cfb77baae548d6a1cfa32c91f040f154394edfb8
Rohit-pawar52 <rkps2580@gmail.com>
2024-11-20 15:35:01 +0530

    -rohit added brand images and dropdown

--- src/Brand/Brand.js
+++ src/Brand/Brand.js
@@ -1,42 +1,37 @@
 import { useEffect, useState } from "react";
-import "../App.css"
+import "../App.css";
 import { FetchBrandApi } from "../Api/BrandApi";
 
+const Brand = () => {
+  const [brandData, setBrandData] = useState([]);
 
-
-const Brand=()=>{
-
-const [brandData , setBrandData] = useState([])
-
-    useEffect(()=>{
-        FetchBrandApi()
-        .then(res=>{
-           
-            // console.log("brand data",res.data)
-            setBrandData(res.data)
-        })
-        .catch(err=>{
-            console.log(err)
-        })
-    })
-    return (
-        <div>
-        {
-            brandData.map((re,index)=>{
-                // console.log("re",re)
-                return <>
-                <img
-                src={`https://eye-care.developmentalphawizz.com/storage/app/public/brands/${re.image}`}
-                alt=""
-                className="product-image"
+  useEffect(() => {
+    FetchBrandApi()
+      .then((res) => {
+        console.log("brand data", res.data);
+        setBrandData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [1]);
+  return (
+    <div className="container-fluid py-4">
+        <p className="h3 font-bolder">BRANDS</p>
+      <div className="d-flex flex-wrap align-items-center gap-2">
+        {brandData.map((brand, index) => {
+          return (
+            <div key="index" className="brand-image-container d-grid align-items-center">
+              <img
+                src={`https://eye-care.developmentalphawizz.com/storage/app/public/brand/${brand.image}`}
+                alt="Glasses 1"
+                className="brand-image"
               />
-              </>
-              
-            })
-        }
-        
-        Brand
-        </div>
-    )
-}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default Brand;
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,129 +1,175 @@
-import "../App.css"
+import "../App.css";
 import { IoBagAdd, IoHome } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
 import { FaCartPlus, FaQuestionCircle, FaUser, FaBars } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import Modal2 from "./Modal2";
-import Logo from '../Image/logo.webp';
+import Logo from "../Image/logo.webp";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Navbar = (props) => {
-    // console.log(props)
-    // incress add to card
-    const incresCard = useSelector((state) => state.allCart.cart);
-    // console.log(incresCard)
+  // console.log(props)
+  // incress add to card
+  const incresCard = useSelector((state) => state.allCart.cart);
+  // console.log(incresCard)
 
+  const [showModal, setShowModal] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false); // State to control sidebar visibility
+  const { loginWithRedirect } = useAuth0();
+  const navigate = useNavigate();
 
-    const [showModal, setShowModal] = useState(false);
-    const [sidebarOpen, setSidebarOpen] = useState(false); // State to control sidebar visibility
-    const { loginWithRedirect } = useAuth0();
-    const navigate = useNavigate();
+  const handleGoNavtoHome = () => {
+    navigate("/");
+    setSidebarOpen(false); // Close the sidebar after navigating
+  };
 
-    const handleGoNavtoHome = () => {
-        navigate("/");
-        setSidebarOpen(false); // Close the sidebar after navigating
-    };
+  const handleBrandProduct = () => {
+    navigate("/brand");
+  };
 
-    const handleBrandProduct=()=>{
-        navigate("/brand")
-    }
+  const handleMobileLogin = () => {
+    navigate("login");
+  };
+  const handleGoJointoLogin = () => {
+    navigate("login");
+  };
+  const handleFlashSale = () => {
+    navigate("flashsale");
+    setSidebarOpen(false); // Close the sidebar after navigating
+  };
 
-    const handleMobileLogin = () => {
-        navigate("login")
-    }
-    const handleGoJointoLogin = () => {
-        navigate("login")
-    }
-    const handleFlashSale = () => {
-        navigate("flashsale");
-        setSidebarOpen(false); // Close the sidebar after navigating
-    };
+  const openProfile = () => {
+    navigate("profile");
+    setSidebarOpen(false); // Close the sidebar after navigating
+  };
 
-    const openProfile = () => {
-        navigate("profile");
-        setSidebarOpen(false); // Close the sidebar after navigating
-    };
+  const handleCartModalClose = () => setShowModal(false);
+  const handleCartModal = () => setShowModal(true);
+  const handleGoLogoFromHome = () => {
+    navigate("mainlogo");
+    setSidebarOpen(false); // Close the sidebar after navigating
+  };
+  const handleContect = () => {
+    navigate("contect");
+    setSidebarOpen(false); // Close the sidebar after navigating
+  };
+  const handleProductfromNavbar = () => {
+    navigate("./product");
+  };
+  const toggleSidebar = () => setSidebarOpen(!sidebarOpen); // Toggle sidebar
 
-    const handleCartModalClose = () => setShowModal(false);
-    const handleCartModal = () => setShowModal(true);
-    const handleGoLogoFromHome = () => {
-        navigate("mainlogo");
-        setSidebarOpen(false); // Close the sidebar after navigating
-    };
-    const handleContect = () => {
-        navigate("contect");
-        setSidebarOpen(false); // Close the sidebar after navigating
-    };
-    const handleProductfromNavbar = () => {
-        navigate("./product")
-    }
-    const toggleSidebar = () => setSidebarOpen(!sidebarOpen); // Toggle sidebar
+  return (
+    <>
+      <div className="nav">
+        {/* Sidebar Toggle Button */}
+        <span className="nav-sidebar-toggle" onClick={toggleSidebar}>
+          <FaBars />
+        </span>
 
-    return (
-        <>
-            <div className="nav">
-                {/* Sidebar Toggle Button */}
-                <span className="nav-sidebar-toggle" onClick={toggleSidebar}>
-                    <FaBars />
-                </span>
+        <img
+          onClick={handleGoLogoFromHome}
+          className="nav-img"
+          src={Logo}
+          alt="Logo"
+        />
 
-                <img onClick={handleGoLogoFromHome} className="nav-img" src={Logo} alt="Logo" />
-
-                <div className="navMenuContainerList">
-                    <div className="NavMenuList">
-                        <div className="hoverList">
-                            <li onClick={handleProductfromNavbar}>Product</li>
-
-                        </div>
-                    </div>
-                    <li>Category</li>
-                    <li onClick={handleBrandProduct}>Brand</li>
-                    <li onClick={handleContect}>Contact</li>
-                    <li onClick={handleFlashSale}>Flash Sale</li>
-                </div>
-                <li onClick={openProfile} className="nav-ques"><FaQuestionCircle /></li>
-                <span className="nav-serch"><CiSearch /></span>
-                <button onClick={handleGoJointoLogin} className="nav-btn">Join</button>
-                <span>{props.userName}</span>
-                <span onClick={handleCartModal} className="nav-cart"><FaCartPlus /></span>
-                <div className="notifaction">{incresCard.length}</div>
+        <div className="navMenuContainerList">
+          <div className="NavMenuList">
+            <div className="hoverList">
+              <li onClick={handleProductfromNavbar}>Product</li>
             </div>
+          </div>
+          <div class="dropdown">
+            <li
+              class="dropdown-toggle"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Category
+            </li>
+            <ul class="dropdown-menu">
+              <li>
+                <a class="dropdown-item" href="#">
+                  Action
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Another action
+                </a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </li>
+            </ul>
+          </div>
+          <li onClick={handleBrandProduct}>Brand</li>
+          <li onClick={handleContect}>Contact</li>
+          <li onClick={handleFlashSale}>Flash Sale</li>
+        </div>
+        <li onClick={openProfile} className="nav-ques">
+          <FaQuestionCircle />
+        </li>
+        <span className="nav-serch">
+          <CiSearch />
+        </span>
+        <button onClick={handleGoJointoLogin} className="nav-btn">
+          Join
+        </button>
+        <span>{props.userName}</span>
+        <span onClick={handleCartModal} className="nav-cart">
+          <FaCartPlus />
+        </span>
+        <div className="notifaction">{incresCard.length}</div>
+      </div>
 
-            {/* Sidebar for small screens */}
-            {sidebarOpen && (
-                <div className="sidebar">
-                    <div className="sidebar-content">
-                        <span className="sidebar-close" onClick={toggleSidebar}>X</span>
-                        <li onClick={handleGoNavtoHome}>Home</li>
-                        {/* <li onClick={openProfile}>Profile</li> */}
-                        <li onClick={handleContect}>Contact</li>
-                        <li onClick={handleFlashSale}>Flash Sale</li>
-                    </div>
-                </div>
-            )}
+      {/* Sidebar for small screens */}
+      {sidebarOpen && (
+        <div className="sidebar">
+          <div className="sidebar-content">
+            <span className="sidebar-close" onClick={toggleSidebar}>
+              X
+            </span>
+            <li onClick={handleGoNavtoHome}>Home</li>
+            {/* <li onClick={openProfile}>Profile</li> */}
+            <li onClick={handleContect}>Contact</li>
+            <li onClick={handleFlashSale}>Flash Sale</li>
+          </div>
+        </div>
+      )}
 
-            {/* Responsive Nav */}
-            <div className="nav-res">
-                <div className="nav-res-1">
-                    <span onClick={handleGoNavtoHome}><IoHome /></span>
-                </div>
-                <div className="nav-res-2">
-                    <span onClick={handleMobileLogin}><FaUser /></span>
-                </div>
-                <div className="nav-res-3">
-                    <span><CiSearch /></span>
-                </div>
-                <div className="nav-res-4">
-                    <div className="nav-res-6">{incresCard.length}</div>
-                    <span onClick={handleCartModal}><FaCartPlus /></span>
-                </div>
-            </div>
+      {/* Responsive Nav */}
+      <div className="nav-res">
+        <div className="nav-res-1">
+          <span onClick={handleGoNavtoHome}>
+            <IoHome />
+          </span>
+        </div>
+        <div className="nav-res-2">
+          <span onClick={handleMobileLogin}>
+            <FaUser />
+          </span>
+        </div>
+        <div className="nav-res-3">
+          <span>
+            <CiSearch />
+          </span>
+        </div>
+        <div className="nav-res-4">
+          <div className="nav-res-6">{incresCard.length}</div>
+          <span onClick={handleCartModal}>
+            <FaCartPlus />
+          </span>
+        </div>
+      </div>
 
-            {showModal && <Modal2 handleCartModalClose={handleCartModalClose} />}
-        </>
-    );
-}
+      {showModal && <Modal2 handleCartModalClose={handleCartModalClose} />}
+    </>
+  );
+};
 
 export default Navbar;
